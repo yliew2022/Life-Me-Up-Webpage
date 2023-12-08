64f8c6aec06b25029f9db22ce18b5dd992b4d665
yliew2022 <yliew2022@fau.edu>
2023-12-07 23:14:00 -0500

    again

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 app.use(express.json())
 
 
-app.use(express.static(__dirname + "/"));
+app.use(express.static(path.join(__dirname, '/')));
 app.use((req,res,next) =>{  
     next()
 })
